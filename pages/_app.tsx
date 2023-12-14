34fdc60a7c6aaac96c26c2e395766c6dad8d7214
Ivan <ivan.golubkov7@gmail.com>
2023-12-14 13:57:27 +0200

    made an edit account button(doesn't work :( )

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import { SessionProvider } from 'next-auth/react';
 import Layout from "@/components/Layout"
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return ( 
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
